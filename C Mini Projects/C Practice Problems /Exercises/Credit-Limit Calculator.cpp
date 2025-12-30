dfb09505df0d468aa871e270c8ec6fe1f4b5907c
Phạm Nguyễn Hoàng Tuấn <tuanpham2601KD@gmail.com>
2025-12-30 14:18:56 +0700

    Document Credit-Limit Calculator with comments
    Added comments and author information to the Credit-Limit Calculator program.

--- C Mini Projects/C Practice Problems /Exercises/Credit-Limit Calculator.cpp
+++ C Mini Projects/C Practice Problems /Exercises/Credit-Limit Calculator.cpp
@@ -1,3 +1,18 @@
+/*(Credit-Limit Calculator) Develop a C program that will determine whether a department
+store customer has exceeded the credit limit on a charge account. For each customer, the following
+facts are available:
+a) Account number
+b) Balance at the beginning of the month
+c) Total of all items charged by this customer this month
+d) Total of all credits applied to this customer's account this month
+e) Allowed credit limit
+The program should input each fact, calculate the new balance (= beginning balance +
+charges – credits), and determine whether the new balance exceeds the customer's credit limit. For
+those customers whose credit limit is exceeded, the program should display the customer's account
+number, credit limit, new balance and the message “Credit limit exceeded.”*/
+
+// PHAM NGUYEN HOANG TUAN
+
 #include<stdio.h>
 
 int main(){
@@ -43,3 +58,4 @@ int main(){
 	}
     return 0;
 }
+
