479fed24738a2acb48411dbde8521a5c30f9f6f3
Phạm Nguyễn Hoàng Tuấn <tuanpham2601KD@gmail.com>
2025-12-31 16:28:24 +0700

    Add files via upload

---
+++ C Mini Projects/C Practice Problems /Exercises/Calculating the Sum of Even Integers.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+	int a;
+	int sum = 0;
+	while(1){
+	printf("Nhap tong can tim: ");
+	scanf("%d", &a);
+	if ( a < 0 ) {
+		printf("Ket thuc");
+		break;
+	}
+	for( int i = 2; i <= a; i+=2){
+		sum = sum + i;
+	}
+	printf("Gia tri tong so chan tu 2 den %d: %d\n",a ,sum);
+	sum = 0;
+	}
+	return 0;
+}
